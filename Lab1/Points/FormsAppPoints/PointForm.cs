2caaf3358e1750d5cc64d9c53e7addcf87d4ed78
Valeriy Kondratenkov <v.kondratenkov@seuslab.ru>
2023-02-05 19:06:52 +0500

    Проект по импорту класса из сторонней библиотеки на .NET Standard в WinForms

---
+++ Lab1/Points/FormsAppPoints/PointForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Threading;
+using PointLib;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+//using System.Text.Json;
+
+namespace FormsAppPoints
+{
+    public partial class PointForm : Form
+    {
+        private Point[] points = null;
+        public PointForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, System.EventArgs e)
+        {
+            points = new Point[5];
+            var rnd = new Random();
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = rnd.Next(3) % 2 == 0 ? new Point() : new Point3D();
+                Thread.Sleep(5);
+            }
+            listBox.DataSource = points;
+        }
+
+        private void btnSort_Click(object sender, System.EventArgs e)
+        {
+            if (points == null)
+                return;
+            Array.Sort(points);
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs = new FileStream(dlg.FileName, FileMode.Create, FileAccess.Write))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        bf.Serialize(fs, points);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        sf.Serialize(fs, points);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        xf.Serialize(fs, points);
+                        break;
+                    case ".json":
+                        //var jf = new JsonSerializer();
+                        //using (var w = new StreamWriter(fs))
+                        //    jf.Serialize(w, points);
+
+                        JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+                        string Serialized = JsonConvert.SerializeObject(points, settings);
+                        using (var jf = new StreamWriter(fs))
+                            jf.Write(Serialized);
+
+                        break;
+
+                }
+            }
+
+
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs = new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        points = (Point[])bf.Deserialize(fs);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        points = (Point[])sf.Deserialize(fs);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        points = (Point[])xf.Deserialize(fs);
+                        break;
+                    case ".json":
+                        //var jf = new JsonSerializer();
+                        //using (var r = new StreamReader(fs))
+                        //    points = (Point[])jf.Deserialize(r,typeof(Point[]));
+
+                        using (var r = new StreamReader(fs))
+                        {
+                            string json = r.ReadToEnd();
+                            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+                            points = JsonConvert.DeserializeObject<Point[]>(json, settings);
+                        }
+
+                        break;
+
+                }
+            }
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+
+
+        }
+    }
+}
---
+++ Lab1/Points/PointLib/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PointLib
+{
+    [Serializable]
+    public class Point : IComparable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        protected Random rnd = new Random(DateTime.Now.Millisecond);
+        public Point()
+        {
+            X = rnd.Next(10);
+            Y = rnd.Next(10);
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public virtual double Metric()
+        {
+            return Math.Sqrt(X*X + Y*Y);
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Point p = (Point)obj;
+            return (int)(Metric() - p.Metric());
+        }
+    }
+}
